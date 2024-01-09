fcd2fb403819b72d16c5ce2a5f07f98e4bae50a9
VuKer <74062885+TDMVu18@users.noreply.github.com>
2024-01-10 01:54:57 +0700

    new crawl files

--- get_friends.py
+++ get_friends.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import re
 
-#Option Chrome Driver ~
+#ghi chú
 
 option = Options()
 option.add_argument("--disable-infobars")
@@ -19,11 +19,11 @@ option.add_experimental_option(
     "prefs", {"profile.default_content_setting_values.notifications": 1}
 )
 
-#set up
+#setup
 
 start_url = 'https://www.facebook.com'
-profile_url = 'https://www.facebook.com/jijivishaphuoc' #url truyền vào, lấy từ người thả react, crawl bạn bè của nó
-PATH = "driver\chromedriver.exe"
+profile_url = 'https://www.facebook.com/profile.php?id=100008402384102'
+PATH = "./chromedriver.exe"
 driver = webdriver.Chrome(chrome_options=option, executable_path=PATH)
 
 #def
@@ -42,15 +42,59 @@ def login(url):
 
 login(start_url)
 
-#if visible: có tag friend mới lấy, k thì thôi :v
-#hiện chưa nghĩ ra nên làm gì vào nếu ko hiện friend, bỏ qua ? đếm friend ?
+#infor DatVilla
+
+def get_about_place(url):
+    url = url + '/about_places'    
+    driver.get(url)
+    driver.maximize_window()
+    # place = driver.find_element(By.XPATH, "//span[@class = 'xt0psk2']").text
+    place = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, "//span[@class = 'xt0psk2']"))
+    ).text
+    return place
+
+def get_gender(url):
+    url = url + '/about_contact_and_basic_info'
+    driver.get(url)
+    driver.maximize_window()
+    try:
+        # gender = driver.find_element(By.XPATH, "//div[@class='x1hq5gj4']/div/div/div[2]/div/div/div/div/div/span").text
+        gender = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.XPATH, "//div[@class='x1hq5gj4']/div/div/div[2]/div/div/div/div/div/span"))
+        ).text
+    except:
+        gender = None
+    return gender
+
+def get_date_of_birth(url):
+    url = url + '/about_contact_and_basic_info'
+    driver.get(url)
+    driver.maximize_window()
+    try:
+        # date_month = driver.find_element(By.XPATH, "//div[@class = 'x1hq5gj4']/../div[3]/div/div/div[2]/div/div/div/div/div/span").text
+        date_month = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.XPATH, "//div[@class = 'x1hq5gj4']/../div[3]/div/div/div[2]/div/div/div/div/div/span"))
+        ).text
+        year = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.XPATH, "//div[@class = 'x1hq5gj4']/../div[3]/div/div/div[2]/div[2]/div/div/div/div/span"))
+        ).text
+    except:
+        date_month = None
+        year = None
+    return date_month, year
+
+
+#if visible
 
 def friend_tab(url):
     url = url
-    driver.get(url + '/friends')
+    driver.get(url)
     driver.maximize_window()
-    driver.execute_script("window.scrollTo(0, 500)") #Scroll down đến khi thấy tag friend ? 
-    test_friend = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div/div/div/div/div/div[2]/div/div/div/div[2]/a[1]")))
+    friendsTab = driver.find_element(By.XPATH,"//span[text()='Bạn bè']")
+    friendsTab.click()
+    driver.execute_script("window.scrollTo(0, 500)")
+    test_friend = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div/div/div/div/div/div[2]/div/div/div/div[2]/a[1]")))
     abc = test_friend.get_attribute("href")
     match = re.search(r'friends_all$', abc)
     if match is not None:
@@ -69,11 +113,11 @@ if check_public == 'visible':
     num_of_loaded_friends = len(_get_friends_list())
     while True:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(5)
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            break
-        last_height = new_height
+        try:
+            WebDriverWait(driver, 2).until(lambda driver: len(_get_friends_list()) > num_of_loaded_friends)
+            num_of_loaded_friends = len(_get_friends_list())
+        except TimeoutException:
+            break  # no more friends loaded
     my_friend = driver.find_elements_by_css_selector(".x1iyjqo2.x1pi30zi [href]")
     friendsList = []
     urlList = []
@@ -84,15 +128,34 @@ if check_public == 'visible':
         urlList.append(url)
         print(friend.text, url)
 
-    newurlList = urlList[3:] 
-    newfriendList = friendsList[3:]
-   
+    # newurlList = urlList[3:] 
+    # newfriendList = friendsList[3:]
+    # placeList = []
+    # genderList = []
+    # dateList = []
+    # yearList = []
+
+    # for target in newurlList:
+    #     place = get_about_place(target)
+    #     gender = get_gender(target)
+    #     date_of_birth = get_date_of_birth(target)[0]
+    #     year = get_date_of_birth(target)[1]
+    #     print(place)
+    #     print(gender)
+    #     print(date_of_birth)
+    #     print(year)
+    #     placeList.append(place)
+    #     genderList.append(gender)
+    #     dateList.append(date_of_birth)
+    #     yearList.append(year)
+
+
 else:
     print("bruhhh")
 
 
 #join dataframe
-# thông tin của 1 cá nhân (name, birthday, gender, friend number(if visible), place) ?
+
 
 
 
